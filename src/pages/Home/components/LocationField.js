f33643bc9a7be31957bc426fd1d90adc7f0aa4ce
Branko  Zivanovic <zivanovic.b1@hotmail.com>
2017-08-17 16:59:39 +0200

    Made reusable component out of StartingPoint and DestinationPoint

---
+++ src/pages/Home/components/LocationField.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import AutoComplete from "./AutoComplete";
+import PropTypes from "prop-types";
+import axios from "axios";
+
+import { buildQueryString } from "../../../helpers/queryStringBuilder";
+// material-ui requires this for its events
+var injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
+
+const BASE_URL = "http://localhost:3000";
+export default class StartingPoint extends Component {
+  state = {
+    value: "",
+    placeId: "",
+    items: []
+  };
+  static propTypes = {
+    updateValue: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired
+  };
+
+  onUpdateInput = value => {
+    let queryString = buildQueryString(value);
+    // getting autocomplete items
+    axios
+      .request({
+        url: BASE_URL + "/api/autocomplete",
+        method: "GET",
+        params: {
+          query: queryString
+        }
+      })
+      .then(response => {
+        this.setState({ items: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    updateParent(this.state.items, value, this.props.updateValue);
+  };
+
+  render() {
+    const { items } = this.state;
+    const { label } = this.props;
+    return (
+      <AutoComplete
+        label={label}
+        items={items}
+        onUpdateInput={this.onUpdateInput}
+      />
+    );
+  }
+}
+
+// whole place object is sent to parent
+function updateParent(items, value, cb) {
+  let obj = items.filter(item => {
+    return item.description === value;
+  })[0];
+  cb(obj ? obj : null);
+}
